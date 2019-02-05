40e18074fead460d71590c4a1a81a40f56676ee7
Boboaca Costel <bobyca2003@yahoo.com>
2019-02-05 09:52:53 +0100

    with x and y separately without dpi

--- Potrace.js
+++ Potrace.js
@@ -1233,31 +1233,31 @@ var Potrace = (function() {
     info.isReady = false;
   }
   
-  function getSVG(size, opt_type) {
+  function getSVG(sizex,sizey, opt_type) {
   
     function path(curve) {
     
       function bezier(i) {
-        var b = 'C ' + (curve.c[i * 3 + 0].x * size).toFixed(3) + ' ' +
-            (curve.c[i * 3 + 0].y * size).toFixed(3) + ',';
-        b += (curve.c[i * 3 + 1].x * size).toFixed(3) + ' ' +
-            (curve.c[i * 3 + 1].y * size).toFixed(3) + ',';
-        b += (curve.c[i * 3 + 2].x * size).toFixed(3) + ' ' +
-            (curve.c[i * 3 + 2].y * size).toFixed(3) + ' ';
+        var b = 'C ' + (curve.c[i * 3 + 0].x * sizex).toFixed(3) + ' ' +
+            (curve.c[i * 3 + 0].y * sizey).toFixed(3) + ',';
+        b += (curve.c[i * 3 + 1].x * sizex).toFixed(3) + ' ' +
+            (curve.c[i * 3 + 1].y * sizey).toFixed(3) + ',';
+        b += (curve.c[i * 3 + 2].x * sizex).toFixed(3) + ' ' +
+            (curve.c[i * 3 + 2].y * sizey).toFixed(3) + ' ';
         return b;
       }
     
       function segment(i) {
-        var s = 'L ' + (curve.c[i * 3 + 1].x * size).toFixed(3) + ' ' +
-            (curve.c[i * 3 + 1].y * size).toFixed(3) + ' ';
-        s += (curve.c[i * 3 + 2].x * size).toFixed(3) + ' ' +
-            (curve.c[i * 3 + 2].y * size).toFixed(3) + ' ';
+        var s = 'L ' + (curve.c[i * 3 + 1].x * sizex).toFixed(3) + ' ' +
+            (curve.c[i * 3 + 1].y * sizey).toFixed(3) + ' ';
+        s += (curve.c[i * 3 + 2].x * sizex).toFixed(3) + ' ' +
+            (curve.c[i * 3 + 2].y * sizey).toFixed(3) + ' ';
         return s;
       }
 
       var n = curve.n, i;
-      var p = 'M' + (curve.c[(n - 1) * 3 + 2].x * size).toFixed(3) +
-          ' ' + (curve.c[(n - 1) * 3 + 2].y * size).toFixed(3) + ' ';
+      var p = 'M' + (curve.c[(n - 1) * 3 + 2].x * sizex).toFixed(3) +
+          ' ' + (curve.c[(n - 1) * 3 + 2].y * sizey).toFixed(3) + ' ';
       for (i = 0; i < n; i++) {
         if (curve.tag[i] === "CURVE") {
           p += bezier(i);
@@ -1269,7 +1269,7 @@ var Potrace = (function() {
       return p;
     }
 
-    var w = bm.w * size, h = bm.h * size,
+    var w = bm.w * sizex, h = bm.h * sizey,
       len = pathlist.length, c, i, strokec, fillc, fillrule;
 
     var svg = '<svg id="svg" version="1.1" width="' + w + '" height="' + h +
